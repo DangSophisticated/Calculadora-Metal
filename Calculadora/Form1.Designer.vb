b7e17a8e58686d6ce320c68727b99f61168864ed
DangSophisticated <nunezdiego993@gmail.com>
2025-03-12 23:29:13 -0400

    Add files via upload

---
+++ Calculadora/Form1.Designer.vb
@@ -0,0 +1,434 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        Label2 = New Label()
+        Label4 = New Label()
+        lbl_titulo = New Label()
+        Label7 = New Label()
+        btn_0 = New Button()
+        txb_panel = New TextBox()
+        btn_1 = New Button()
+        btn_4 = New Button()
+        btn_mas = New Button()
+        btn_5 = New Button()
+        btn_2 = New Button()
+        btn_dec = New Button()
+        btn_6 = New Button()
+        btn_3 = New Button()
+        btn_igual = New Button()
+        btn_por = New Button()
+        btn_div = New Button()
+        btn_C = New Button()
+        btn_7 = New Button()
+        btn_8 = New Button()
+        btn_9 = New Button()
+        btn_men = New Button()
+        PictureBox2 = New PictureBox()
+        l_memoria = New ListBox()
+        Label3 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(106, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 20)
+        Label1.TabIndex = 0
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 9)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(0, 20)
+        Label2.TabIndex = 1
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(185, 9)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(0, 20)
+        Label4.TabIndex = 3
+        ' 
+        ' lbl_titulo
+        ' 
+        lbl_titulo.AutoSize = True
+        lbl_titulo.BackColor = Color.White
+        lbl_titulo.BorderStyle = BorderStyle.Fixed3D
+        lbl_titulo.FlatStyle = FlatStyle.Popup
+        lbl_titulo.Font = New Font("SEGA LOGO FONT", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lbl_titulo.ForeColor = Color.RoyalBlue
+        lbl_titulo.Location = New Point(67, 29)
+        lbl_titulo.Name = "lbl_titulo"
+        lbl_titulo.Size = New Size(247, 30)
+        lbl_titulo.TabIndex = 4
+        lbl_titulo.Text = "Calculadora"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(145, 43)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(0, 20)
+        Label7.TabIndex = 6
+        ' 
+        ' btn_0
+        ' 
+        btn_0.Cursor = Cursors.Hand
+        btn_0.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_0.Location = New Point(76, 331)
+        btn_0.Name = "btn_0"
+        btn_0.Size = New Size(40, 40)
+        btn_0.TabIndex = 7
+        btn_0.Text = "0"
+        btn_0.UseVisualStyleBackColor = True
+        ' 
+        ' txb_panel
+        ' 
+        txb_panel.Font = New Font("Sega Arcade Font", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txb_panel.Location = New Point(76, 100)
+        txb_panel.Name = "txb_panel"
+        txb_panel.Size = New Size(163, 36)
+        txb_panel.TabIndex = 8
+        txb_panel.Text = "0"
+        txb_panel.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' btn_1
+        ' 
+        btn_1.Cursor = Cursors.Hand
+        btn_1.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_1.Location = New Point(76, 272)
+        btn_1.Name = "btn_1"
+        btn_1.Size = New Size(40, 40)
+        btn_1.TabIndex = 12
+        btn_1.Text = "1"
+        btn_1.UseVisualStyleBackColor = True
+        ' 
+        ' btn_4
+        ' 
+        btn_4.Cursor = Cursors.Hand
+        btn_4.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_4.Location = New Point(76, 214)
+        btn_4.Name = "btn_4"
+        btn_4.Size = New Size(40, 40)
+        btn_4.TabIndex = 20
+        btn_4.Text = "4"
+        btn_4.UseVisualStyleBackColor = True
+        ' 
+        ' btn_mas
+        ' 
+        btn_mas.Cursor = Cursors.Hand
+        btn_mas.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_mas.Location = New Point(260, 100)
+        btn_mas.Name = "btn_mas"
+        btn_mas.Size = New Size(40, 40)
+        btn_mas.TabIndex = 31
+        btn_mas.Text = "+"
+        btn_mas.UseVisualStyleBackColor = True
+        ' 
+        ' btn_5
+        ' 
+        btn_5.Cursor = Cursors.Hand
+        btn_5.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_5.Location = New Point(137, 214)
+        btn_5.Name = "btn_5"
+        btn_5.Size = New Size(40, 40)
+        btn_5.TabIndex = 36
+        btn_5.Text = "5"
+        btn_5.UseVisualStyleBackColor = True
+        ' 
+        ' btn_2
+        ' 
+        btn_2.Cursor = Cursors.Hand
+        btn_2.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_2.Location = New Point(137, 272)
+        btn_2.Name = "btn_2"
+        btn_2.Size = New Size(40, 40)
+        btn_2.TabIndex = 34
+        btn_2.Text = "2"
+        btn_2.UseVisualStyleBackColor = True
+        ' 
+        ' btn_dec
+        ' 
+        btn_dec.Cursor = Cursors.Hand
+        btn_dec.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_dec.Location = New Point(137, 331)
+        btn_dec.Name = "btn_dec"
+        btn_dec.Size = New Size(40, 40)
+        btn_dec.TabIndex = 32
+        btn_dec.Text = "."
+        btn_dec.UseVisualStyleBackColor = True
+        ' 
+        ' btn_6
+        ' 
+        btn_6.Cursor = Cursors.Hand
+        btn_6.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_6.Location = New Point(199, 214)
+        btn_6.Name = "btn_6"
+        btn_6.Size = New Size(40, 40)
+        btn_6.TabIndex = 42
+        btn_6.Text = "6"
+        btn_6.UseVisualStyleBackColor = True
+        ' 
+        ' btn_3
+        ' 
+        btn_3.Cursor = Cursors.Hand
+        btn_3.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_3.Location = New Point(199, 272)
+        btn_3.Name = "btn_3"
+        btn_3.Size = New Size(40, 40)
+        btn_3.TabIndex = 40
+        btn_3.Text = "3"
+        btn_3.UseVisualStyleBackColor = True
+        ' 
+        ' btn_igual
+        ' 
+        btn_igual.Cursor = Cursors.Hand
+        btn_igual.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_igual.Location = New Point(199, 331)
+        btn_igual.Name = "btn_igual"
+        btn_igual.Size = New Size(40, 40)
+        btn_igual.TabIndex = 38
+        btn_igual.Text = "="
+        btn_igual.UseVisualStyleBackColor = True
+        ' 
+        ' btn_por
+        ' 
+        btn_por.Cursor = Cursors.Hand
+        btn_por.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_por.Location = New Point(260, 214)
+        btn_por.Name = "btn_por"
+        btn_por.Size = New Size(40, 40)
+        btn_por.TabIndex = 48
+        btn_por.Text = "*"
+        btn_por.UseVisualStyleBackColor = True
+        ' 
+        ' btn_div
+        ' 
+        btn_div.Cursor = Cursors.Hand
+        btn_div.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_div.Location = New Point(260, 272)
+        btn_div.Name = "btn_div"
+        btn_div.Size = New Size(40, 40)
+        btn_div.TabIndex = 46
+        btn_div.Text = "/"
+        btn_div.UseVisualStyleBackColor = True
+        ' 
+        ' btn_C
+        ' 
+        btn_C.Cursor = Cursors.Hand
+        btn_C.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_C.Location = New Point(260, 331)
+        btn_C.Name = "btn_C"
+        btn_C.Size = New Size(40, 40)
+        btn_C.TabIndex = 44
+        btn_C.Text = "C"
+        btn_C.UseVisualStyleBackColor = True
+        ' 
+        ' btn_7
+        ' 
+        btn_7.Cursor = Cursors.Hand
+        btn_7.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_7.Location = New Point(76, 156)
+        btn_7.Name = "btn_7"
+        btn_7.Size = New Size(40, 40)
+        btn_7.TabIndex = 49
+        btn_7.Text = "7"
+        btn_7.UseVisualStyleBackColor = True
+        ' 
+        ' btn_8
+        ' 
+        btn_8.Cursor = Cursors.Hand
+        btn_8.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_8.Location = New Point(137, 156)
+        btn_8.Name = "btn_8"
+        btn_8.Size = New Size(40, 40)
+        btn_8.TabIndex = 51
+        btn_8.Text = "8"
+        btn_8.UseVisualStyleBackColor = True
+        ' 
+        ' btn_9
+        ' 
+        btn_9.Cursor = Cursors.Hand
+        btn_9.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_9.Location = New Point(199, 156)
+        btn_9.Name = "btn_9"
+        btn_9.Size = New Size(40, 40)
+        btn_9.TabIndex = 53
+        btn_9.Text = "9"
+        btn_9.UseVisualStyleBackColor = True
+        ' 
+        ' btn_men
+        ' 
+        btn_men.Cursor = Cursors.Hand
+        btn_men.Font = New Font("Sega Arcade Font", 11.999999F)
+        btn_men.Location = New Point(260, 156)
+        btn_men.Name = "btn_men"
+        btn_men.Size = New Size(40, 40)
+        btn_men.TabIndex = 55
+        btn_men.Text = "-"
+        btn_men.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox2.BackgroundImageLayout = ImageLayout.None
+        PictureBox2.Cursor = Cursors.Hand
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(15, 377)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(162, 198)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 57
+        PictureBox2.TabStop = False
+        ' 
+        ' l_memoria
+        ' 
+        l_memoria.BorderStyle = BorderStyle.FixedSingle
+        l_memoria.Cursor = Cursors.Hand
+        l_memoria.Font = New Font("Sega Arcade Font", 11.999999F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        l_memoria.ForeColor = Color.DarkGoldenrod
+        l_memoria.FormattingEnabled = True
+        l_memoria.ItemHeight = 17
+        l_memoria.Location = New Point(341, 87)
+        l_memoria.Name = "l_memoria"
+        l_memoria.Size = New Size(240, 274)
+        l_memoria.TabIndex = 58
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.White
+        Label3.BorderStyle = BorderStyle.Fixed3D
+        Label3.FlatStyle = FlatStyle.Popup
+        Label3.Font = New Font("SEGA LOGO FONT", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.RoyalBlue
+        Label3.Location = New Point(372, 29)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(179, 30)
+        Label3.TabIndex = 59
+        Label3.Text = "Memoria"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.BackColor = Color.White
+        Label5.Font = New Font("Segoe UI Symbol", 19.8000011F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label5.ForeColor = Color.White
+        Label5.Location = New Point(60, 21)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(260, 46)
+        Label5.TabIndex = 60
+        Label5.Text = ".............................."
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.BackColor = Color.White
+        Label6.Font = New Font("Segoe UI Symbol", 19.8000011F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label6.ForeColor = Color.White
+        Label6.Location = New Point(363, 21)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(196, 46)
+        Label6.TabIndex = 61
+        Label6.Text = "......................"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(593, 587)
+        Controls.Add(Label3)
+        Controls.Add(l_memoria)
+        Controls.Add(PictureBox2)
+        Controls.Add(btn_men)
+        Controls.Add(btn_9)
+        Controls.Add(btn_8)
+        Controls.Add(btn_7)
+        Controls.Add(btn_por)
+        Controls.Add(btn_div)
+        Controls.Add(btn_C)
+        Controls.Add(btn_6)
+        Controls.Add(btn_3)
+        Controls.Add(btn_igual)
+        Controls.Add(btn_5)
+        Controls.Add(btn_2)
+        Controls.Add(btn_dec)
+        Controls.Add(btn_mas)
+        Controls.Add(btn_4)
+        Controls.Add(btn_1)
+        Controls.Add(txb_panel)
+        Controls.Add(btn_0)
+        Controls.Add(Label7)
+        Controls.Add(lbl_titulo)
+        Controls.Add(Label4)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Label5)
+        Controls.Add(Label6)
+        Name = "Form1"
+        Text = "HUE HUE HUE"
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lbl_titulo As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents btn_0 As Button
+    Friend WithEvents txb_panel As TextBox
+    Friend WithEvents btn_1 As Button
+    Friend WithEvents btn_4 As Button
+    Friend WithEvents btn_mas As Button
+    Friend WithEvents btn_5 As Button
+    Friend WithEvents btn_2 As Button
+    Friend WithEvents btn_dec As Button
+    Friend WithEvents btn_6 As Button
+    Friend WithEvents btn_3 As Button
+    Friend WithEvents btn_igual As Button
+    Friend WithEvents btn_por As Button
+    Friend WithEvents btn_div As Button
+    Friend WithEvents btn_C As Button
+    Friend WithEvents btn_7 As Button
+    Friend WithEvents btn_8 As Button
+    Friend WithEvents btn_9 As Button
+    Friend WithEvents btn_men As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents l_memoria As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+
+End Class
---
+++ Calculadora/Form1.vb
@@ -0,0 +1,169 @@
+﻿Public Class Form1
+
+    Dim calc As String
+
+    Dim resultado As Nullable(Of Double) = Nothing
+
+    Dim valor2 As Nullable(Of Double) = Nothing
+
+    Dim operador As Boolean
+
+    Public Sub evaluaycalcula()
+        operador = True
+        valor2 = Val(txb_panel.Text)
+        If resultado IsNot Nothing Then
+            Select Case calc
+                Case "+"
+                    resultado += valor2
+                Case "-"
+                    resultado -= valor2
+                Case "*"
+                    resultado *= valor2
+                Case "/"
+                    resultado /= valor2
+            End Select
+            txb_panel.Text = resultado
+        Else
+            resultado = valor2
+
+        End If
+    End Sub
+
+    Public Sub restriccion()
+
+        If operador = True Then
+            txb_panel.Text = ""
+            operador = False
+
+        ElseIf txb_panel.Text = "0" Then
+            txb_panel.Text = ""
+        End If
+
+    End Sub
+
+
+
+    Private Sub btn_0_Click(sender As Object, e As EventArgs) Handles btn_0.Click
+        restriccion()
+        txb_panel.Text &= "0"
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav")
+    End Sub
+
+    Private Sub btn_dec_Click(sender As Object, e As EventArgs) Handles btn_dec.Click
+        If InStr(txb_panel.Text, ".", CompareMethod.Text) = 0 Then
+            txb_panel.Text &= "."
+        End If
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav")
+    End Sub
+
+    Private Sub btn_igual_Click(sender As Object, e As EventArgs) Handles btn_igual.Click
+        evaluaycalcula()
+        calc = ""
+
+        l_memoria.Items.Add(resultado)
+
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic cd jump sound.wav")
+    End Sub
+
+    Private Sub btn_C_Click(sender As Object, e As EventArgs) Handles btn_C.Click
+        txb_panel.Text = "0"
+        resultado = Nothing
+        valor2 = Nothing
+
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic The Hedgehog Sonic Getting Hit sound effect.wav")
+
+    End Sub
+
+    Private Sub btn_1_Click(sender As Object, e As EventArgs) Handles btn_1.Click
+        restriccion()
+        txb_panel.Text &= "1"
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav")
+    End Sub
+
+    Private Sub btn_2_Click(sender As Object, e As EventArgs) Handles btn_2.Click
+        restriccion()
+        txb_panel.Text &= "2"
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav")
+    End Sub
+
+    Private Sub btn_3_Click(sender As Object, e As EventArgs) Handles btn_3.Click
+        restriccion()
+        txb_panel.Text &= "3"
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav")
+    End Sub
+
+    Private Sub btn_div_Click(sender As Object, e As EventArgs) Handles btn_div.Click
+        evaluaycalcula()
+        calc = "/"
+
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic cd jump sound.wav")
+    End Sub
+
+    Private Sub btn_4_Click(sender As Object, e As EventArgs) Handles btn_4.Click
+        restriccion()
+        txb_panel.Text &= "4"
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav")
+    End Sub
+
+    Private Sub btn_5_Click(sender As Object, e As EventArgs) Handles btn_5.Click
+        restriccion()
+        txb_panel.Text &= "5"
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav")
+    End Sub
+
+    Private Sub btn_6_Click(sender As Object, e As EventArgs) Handles btn_6.Click
+        restriccion()
+        txb_panel.Text &= "6"
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav")
+    End Sub
+
+    Private Sub btn_por_Click(sender As Object, e As EventArgs) Handles btn_por.Click
+        evaluaycalcula()
+        calc = "*"
+
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic cd jump sound.wav")
+    End Sub
+
+    Private Sub btn_7_Click(sender As Object, e As EventArgs) Handles btn_7.Click
+        restriccion()
+        txb_panel.Text &= "7"
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav")
+    End Sub
+
+    Private Sub btn_8_Click(sender As Object, e As EventArgs) Handles btn_8.Click
+        restriccion()
+        txb_panel.Text &= "8"
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav")
+
+    End Sub
+
+    Private Sub btn_9_Click(sender As Object, e As EventArgs) Handles btn_9.Click
+        restriccion()
+        txb_panel.Text &= "9"
+
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic Ring - Sound Effect (HD).wav", AudioPlayMode.Background)
+    End Sub
+
+    Private Sub btn_men_Click(sender As Object, e As EventArgs) Handles btn_men.Click
+        evaluaycalcula()
+        calc = "-"
+
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic cd jump sound.wav", AudioPlayMode.Background)
+    End Sub
+
+    Private Sub btn_mas_Click(sender As Object, e As EventArgs) Handles btn_mas.Click
+        evaluaycalcula()
+        calc = "+"
+
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\Sonic cd jump sound.wav", AudioPlayMode.Background)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox2.BackColor = Color.Transparent
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        My.Computer.Audio.Play("C:\Users\Hp\Downloads\HueHueHue  HeyHeyHey  Bad Future Version (Clean Version) - Sonic the Hedgehog CD.wav", AudioPlayMode.Background)
+    End Sub
+End Class
